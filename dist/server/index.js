9960199279ba7dfc9628a3f4b82a5634176b0015
Moncef STITI <moncef.stiti@etu.utc.fr>
2026-08-04 16:06:59 +0200

    Fix public site root route

--- dist/server/index.js
+++ dist/server/index.js
@@ -9,8 +9,6 @@ export default {
       });
     }
 
-    const url = new URL(request.url);
-    if (url.pathname === "/") url.pathname = "/index.html";
-    return env.ASSETS.fetch(new Request(url, request));
+    return env.ASSETS.fetch(request);
   },
 };
